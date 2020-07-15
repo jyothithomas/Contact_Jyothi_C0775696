d48eeaac5ee344d374f85775dfbffaf5e1fa3a18
Jyothithomas <jyothithomas.05@gmail.com>
2020-07-15 19:02:43 -0400

    model class added

---
+++ app/src/main/java/com/example/contact_jyothi_c0775696/activities/AddContactActivity.java
@@ -0,0 +1,15 @@
+package com.example.contact_jyothi_c0775696.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.contact_jyothi_c0775696.R;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
